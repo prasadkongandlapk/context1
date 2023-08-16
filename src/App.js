7994cbc87b1d96576a921936a88bff78cb60dd84
prasadkongandlapk <prasadkongandlapk@gmail.com>
2023-08-16 09:37:49 +0000

    fdlkj

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import './App.css'
 class App extends Component {
   state = {showContent: true, showLeftNavbar: true, showRightNavbar: true}
 
+  onToggleShowContent = () => {
+    this.setState(prevState => ({showContext: !prevState.showContent}))
+  }
+
+  onToggleShowLeftNavbar = () => {
+    this.setState(prevState => ({
+      showLeftNavbar: !prevState.showLeftNavbar,
+    }))
+  }
+
+  onToggleShowRightNavbar = () => {
+    this.setState(prevState => ({
+      showRightNavbar: !prevState.showRightNavbar,
+    }))
+  }
+
   render() {
     return (
       <div className="bg">
--- src/components/ConfigurationController/index.js
+++ src/components/ConfigurationController/index.js
@@ -4,26 +4,32 @@ import ConfigurationContext from '../../context/ConfigurationContext'
 const ConfigurationControl = () => (
   <ConfigurationContext.Consumer>
     {value => {
-      const {showContent, showLeftNavbar, showRightNavbar} = value
+      const {
+        showContent,
+        showLeftNavbar,
+        showRightNavbar,
+        onToggleShowContext,
+        onToggleShowLeftNavbar,
+        onToggleShowRightNavbar,
+      } = value
+
       const onShowContent = () => {
-        this.setState(prevState => ({showContext: !prevState.showContent}))
+        onToggleShowContext()
       }
+
       const onShowLeftNavbar = () => {
-        this.setState(prevState => ({
-          showLeftNavbar: !prevState.showLeftNavbar,
-        }))
+        onToggleShowLeftNavbar()
       }
+
       const onShowRightNavbar = () => {
-        this.setState(prevState => ({
-          showRightNavbar: !prevState.showRightNavbar,
-        }))
+        onToggleShowRightNavbar()
       }
 
       return (
         <div className="config-bg">
           <div className="check-bg">
             <input
-              value=""
+              value={showContent}
               id="context"
               type="checkbox"
               onChange={onShowContent}
@@ -31,11 +37,21 @@ const ConfigurationControl = () => (
             <label htmlFor="context">Context</label>
           </div>
           <div className="check-bg">
-            <input id="left" type="checkbox" onChange={onShowLeftNavbar} />
+            <input
+              value={showLeftNavbar}
+              id="left"
+              type="checkbox"
+              onChange={onShowLeftNavbar}
+            />
             <label htmlFor="left">Left Navbar</label>
           </div>
           <div className="check-bg">
-            <input id="right" type="checkbox" onChange={onShowRightNavbar} />
+            <input
+              value={showRightNavbar}
+              id="right"
+              type="checkbox"
+              onChange={onShowRightNavbar}
+            />
             <label htmlFor="right">Right NavBar</label>
           </div>
         </div>
